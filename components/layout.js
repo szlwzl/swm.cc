213e313035af9e2379e165720c39b24b0737e645
Stevie McCullough <stephen.mccullough@gmail.com>
2023-02-23 22:48:59 +0000

    #3 - Added footer. Also added tailwind css.

--- components/layout.js
+++ components/layout.js
@@ -52,14 +52,22 @@ export default function Layout({ children, home, about }) {
           <Link href="/">← Back to home</Link>
         </div>
       )}
-      
-        <div className={styles.footer}>
-      {!about && (
-          <Link href="/about">About</Link>
-      )}
+
+      <div className={styles.footer}>
+        {!about && <Link href="/about">About</Link>}
+      </div>
+
+      <footer class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+        <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
+          © 2023 Made with ❤️ by
+          <a
+            class="text-neutral-800 dark:text-neutral-400"
+            href="/"
+          >
+            &nbsp;Stephen McCullough
+          </a>
         </div>
-
-
+      </footer>
     </div>
   );
 }
