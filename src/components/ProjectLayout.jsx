6caef4603bad81f798d47e2d7f4b4754697aacc2
Stevie McCullough <stephen.mccullough@gmail.com>
2023-03-05 23:13:41 +0000

    🔨 - Started to introduce a template project page.

--- src/components/ProjectLayout.jsx
+++ src/components/ProjectLayout.jsx
@@ -18,7 +18,18 @@ function ArrowLeftIcon(props) {
   )
 }
 
-export function ArticleLayout({
+function LinkIcon(props) {
+    return (
+      <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+        <path
+          d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
+          fill="currentColor"
+        />
+      </svg>
+    )
+  }
+
+export function ProjectLayout({
   children,
   meta,
   isRssFeed = false,
@@ -33,7 +44,7 @@ export function ArticleLayout({
   return (
     <>
       <Head>
-        <title>{`${meta.title} - Articles - Stephen McCullough`}</title>
+        <title>{`${meta.title} - Projects - Stephen McCullough`}</title>
         <meta name="description" content={meta.description} />
       </Head>
       <Container className="mt-16 lg:mt-32">
@@ -43,7 +54,7 @@ export function ArticleLayout({
               <button
                 type="button"
                 onClick={() => router.back()}
-                aria-label="Go back to articles"
+                aria-label="Go back to projects"
                 className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
               >
                 <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
@@ -54,16 +65,14 @@ export function ArticleLayout({
                 <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
                   {meta.title}
                 </h1>
-                <time
-                  dateTime={meta.date}
-                  className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
-                >
-                  <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-                  <span className="ml-3">{formatDate(meta.date)}</span>
-                </time>
               </header>
               <Prose className="mt-8">{children}</Prose>
             </article>
+            <h2>Links</h2> 
+            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                <LinkIcon className="h-6 w-6 flex-none" />
+                <span className="ml-2">Example</span>
+              </p>
           </div>
         </div>
       </Container>
--- src/pages/projects/pulp.jsx
+++ src/pages/projects/pulp.jsx
@@ -6,77 +6,48 @@ import { SimpleLayout } from '@/components/SimpleLayout'
 import logoSwmcc from '@/images/logos/swmcc.svg'
 import logoTheMcCulloughs from '@/images/logos/the-mcculloughs.svg'
 import logoPulp from '@/images/logos/pulp.svg'
-import logoTvDB from '@/images/logos/tvdb.svg'
-import logoLinks from '@/images/logos/links.svg'
-import logoBookMarks from '@/images/logos/bookmarks.svg'
-import logoWhatIsOnTheTv from '@/images/logos/whatisonthetv.svg'
-import logoMaProject from '@/images/logos/maproject.svg'
 
 
 const projects = [
   {
-    name: 'the-mcculloughs.org',
-    description:
-      'A digitised family slide, photo and video album for my family.',
-    link: { href: '#' },
-    logo: logoTheMcCulloughs,
-  },
-  {
     name: 'swm.cc',
     description:
       'My personal website, built with Next.js, Tailwind CSS and TypeScript.',
-    link: { href: '/projects/swmcc' },
+    link: { href: 'https://www.github.com/swmcc/swm.cc' },
     logo: logoSwmcc,
+  },
+  {
+    name: 'the-mcculloughs.org',
+    description:
+      'A digitised family slide, photo and video album for my family.',
+    link: { href: 'https://www.github.com/swmcc/swm.cc' },
+    logo: logoTheMcCulloughs,
   },
   {
     name: 'pulp',
     description:
       'My utility app. Just a kitchen sink of stuffs I have written.',
-    link: { href: '#' },
+    link: { href: 'https://www.github.com/swmcc/swm.cc' },
     logo: logoPulp,
-  },
-  {
-    name: 'tvdb',
-    description:
-      'TheTVDB is one of the longest-running community-driven TV and Movie databases.',
-    link: { href: '#' },
-    logo: logoTvDB,
-  },
-  {
-    name: 'links',
-    description:
-      'All the kool kidz have them, so I made one too.',
-    link: { href: '#' },
-    logo: logoLinks,
-  },
-  {
-    name: 'bookmarks',
-    description:
-      'My bookmarks app. I use this to store all my bookmarks, and then sync them to various devices.',
-    link: { href: '#' },
-    logo: logoBookMarks,
-  },
-  {
-    name: 'whatisonthe.tv',
-    description:
-      'COMING SOON....',
-    link: { href: '#' },
-    logo: logoWhatIsOnTheTv,
-  },
-  {
-    name: 'Ma Project',
-    description:
-      'My Project held in the cloud at GitHub.',
-    link: { href: 'https://github.com/users/swmcc/projects/6' },
-    logo: logoMaProject,
-  },
+  }
 ]
+
+function LinkIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
 
 export default function Projects() {
   return (
     <>
       <Head>
-        <title>Projects - Stephen McCullough</title>
+        <title>Pulp - Projects - Stephen McCullough</title>
         <meta
           name="description"
           content="Things I’ve made, making or thinking about."
