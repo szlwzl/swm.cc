37a09f3298cf7a4d1fe27b0aa1270591ce1b272c
Stevie McCullough <stephen.mccullough@gmail.com>
2023-03-15 08:32:35 +0000

    💄 - #16 - Added the swm.cc project page.

--- src/components/ProjectLayout.jsx
+++ src/components/ProjectLayout.jsx
@@ -68,11 +68,7 @@ export function ProjectLayout({
               </header>
               <Prose className="mt-8">{children}</Prose>
             </article>
-            <h2>Links</h2> 
-            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">Example</span>
-              </p>
+
           </div>
         </div>
       </Container>
